243f6a0b4c42dca19e4d61dc4547e3b907885800
attiz <zattila97@yahoo.com>
2016-12-09 11:40:02 +0200

    first
    first

---
+++ MyApplication/app/src/main/java/zoldattila/example/com/widgetproject/MainActivity.java
@@ -0,0 +1,13 @@
+package zoldattila.example.com.widgetproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
